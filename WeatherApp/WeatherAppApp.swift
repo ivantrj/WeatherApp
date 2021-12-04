905b3cb7b02be38afa2b955d8f5bcbffeca6c8ce
Ivan Trj <ivantrajanovski@gmail.com>
2021-12-04 16:10:38 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ivan Trajanovski on 2021. 12. 04..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
