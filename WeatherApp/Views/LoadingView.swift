62e85b750afac427e96df3745b7b2839bbaebdfb
Ivan Trj <ivantrajanovski@gmail.com>
2021-12-04 21:08:02 +0100

    push mvp

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Ivan Trajanovski on 2021. 12. 04..
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
